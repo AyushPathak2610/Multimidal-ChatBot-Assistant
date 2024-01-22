7e99cdc08efd6ff90e7f866bbcdd18d267c9adb9
Ayush <ayushpathak2610@gmail.com>
2024-01-23 00:36:15 +0530

    FIles commit
    Multimodal ChatBot Assistant made using Python, Streamlit, LLaVA, Whisper AI, Chroma DB

---
+++ app.py
@@ -0,0 +1,134 @@
+import streamlit as st
+from llm_chains import load_normal_chain, load_pdf_chat_chain
+from streamlit_mic_recorder import mic_recorder
+from langchain.memory import StreamlitChatMessageHistory
+from audio_handler import transcribe_audio
+from pdf_handler import add_documents_to_db
+from image_handler import handle_image
+from html_templates import get_bot_template, get_user_template, css
+from utils import save_chat_history_json, get_timestamp, load_chat_history_json
+import yaml
+import os
+
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+def clear_input_field():
+    if st.session_state.user_question == "":
+        st.session_state.user_question = st.session_state.user_input
+        st.session_state.user_input = ""
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+    
+def load_chain(chat_history):
+    if st.session_state.pdf_chat:
+        return load_pdf_chat_chain(chat_history)
+    return load_normal_chain(chat_history)
+
+def toggle_pdf_chat():
+    st.session_state.pdf_chat = True
+
+def save_chat_history():
+    if st.session_state.history != []:
+        if st.session_state.session_key == "new_session":
+            st.session_state.new_session_key = get_timestamp() + ".json"
+            save_chat_history_json(st.session_state.history, config["chat_history_path"] + st.session_state.new_session_key)
+        else:
+            save_chat_history_json(st.session_state.history, config["chat_history_path"] + st.session_state.session_key)
+
+def track_index():
+    st.session_state.session_index_tracker = st.session_state.session_key
+
+def main():
+    st.title("ChatBot Assistant")
+    st.write(css, unsafe_allow_html=True)
+    
+    chat_container = st.container()
+    st.sidebar.title("Chat Sessions")
+    chat_sessions = ["new_session"] + os.listdir(config["chat_history_path"])
+
+    if "send_input" not in st.session_state:
+        st.session_state.session_key = "new_session"
+        st.session_state.send_input = False
+        st.session_state.user_question = ""
+        st.session_state.new_session_key = None
+        st.session_state.session_index_tracker = "new_session"
+    if st.session_state.session_key == "new_session" and st.session_state.new_session_key != None:
+        st.session_state.session_index_tracker = st.session_state.new_session_key
+        st.session_state.new_session_key = None
+
+
+    index = chat_sessions.index(st.session_state.session_index_tracker)
+    st.sidebar.selectbox("Select a chat session", chat_sessions, key="session_key", index=index, on_change=track_index)
+    st.sidebar.toggle("PDF Chat", key="pdf_chat", value=False)
+
+    if st.session_state.session_key != "new_session":
+        st.session_state.history = load_chat_history_json(config["chat_history_path"] + st.session_state.session_key)
+    else:
+        st.session_state.history = []
+    
+    chat_history = StreamlitChatMessageHistory(key="history")
+    llm_chain = load_chain(chat_history)
+    
+    user_input = st.text_input("Type your message here", key="user_input", on_change=set_send_input)
+    
+    voice_recording_column, send_button_column = st.columns(2)
+    
+    chat_container = st.container()
+    
+    with voice_recording_column:
+        voice_recording=mic_recorder(start_prompt="Start recording",stop_prompt="Stop recording", just_once=True)
+    with send_button_column:
+        send_button = st.button("Send", key="send_button", on_click=clear_input_field)
+    
+    uploaded_audio = st.sidebar.file_uploader("Upload an audio file", type=["wav", "mp3", "ogg"])
+    uploaded_image = st.sidebar.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
+    uploaded_pdf = st.sidebar.file_uploader("Upload a pdf file", accept_multiple_files=True, key="pdf_upload", type=["pdf"], on_change=toggle_pdf_chat)
+
+    if uploaded_pdf:
+        with st.spinner("Processing pdf..."):
+            add_documents_to_db(uploaded_pdf)
+    
+    if uploaded_audio:
+        transcribed_audio = transcribe_audio(uploaded_audio.getvalue())
+        llm_chain.run("Summarise the text: " + transcribed_audio)
+        uploaded_audio = None
+    
+    if voice_recording:
+        transcribed_audio = transcribe_audio(voice_recording["bytes"])
+        print(transcribed_audio)
+        llm_chain = load_chain(chat_history)
+        llm_chain.run(transcribed_audio)
+    
+    if send_button or st.session_state.send_input:
+        if uploaded_image:
+            with st.spinner("Processing image..."):
+                user_message = "Describe this image in detail please."
+                if st.session_state.user_question != "":
+                    user_message = st.session_state.user_question
+                    st.session_state.user_question = ""
+                llm_answer = handle_image(uploaded_image.getvalue(), st.session_state.user_question)
+                chat_history.add_user_message(user_message)
+                chat_history.add_ai_message(llm_answer)
+
+        if st.session_state.user_question != "":
+            llm_chain = load_chain(chat_history)
+            llm_response = llm_chain.run(st.session_state.user_question)
+            st.session_state.user_question = ""
+
+        st.session_state.send_input = False
+                
+    if chat_history.messages!= []:
+        with chat_container:
+            st.write("Chat History: ")
+            for message in reversed(chat_history.messages):
+                st.chat_message(message.type).write(message.content)
+                
+    
+    save_chat_history()
+    # print(chat_history.messages[0].dict())
+        
+if __name__=="__main__":
+    main()
---
+++ prompt_templates.py
@@ -0,0 +1,4 @@
+memory_prompt_template = """<s>[INST] You are an AI chatbot having converstaion with a human. Answer his question.
+    Previous conversation: {history}
+    Human: {human_input}
+    [/INST]"""
